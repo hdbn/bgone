e0ae952370ba6d136e461b0713f0f8cf5d4cf94b
Keisuke Goto <keisuke.gotou@inf.kyushu-u.ac.jp>
2013-10-04 11:18:13 +0900

    modify comment

--- src/bgCommon.hpp
+++ src/bgCommon.hpp
@@ -1,18 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
 // bgCommon.hpp
-//   common routines for lz factorization BGS, iBGS, BGL, iBGL, BGT, iBGT
-////////////////////////////////////////////////////////////////////////////////
-// Copyright 2012 Hideo Bannai & Keisuke Goto
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// General Public License for more details.
 ////////////////////////////////////////////////////////////////////////////////
 
 #ifndef __LZBG_COMMON_HPP__
@@ -23,36 +10,71 @@
 #include <climits>
 
 namespace LZShuffle {
-  const unsigned int EMPTY = UINT_MAX; // same definition of EMPTY in sa2phi.hpp
+  const unsigned int EMPTY = UINT_MAX; /**< same definition of EMPTY in sa2phi.hpp */
 
   extern bool checkResult;
   extern bool count_num_factors_only;
   
   enum FLAGS {
-    DOUBLE_SA = 1, // allocate double required memory for suffix array
-    SENTINEL = 2 // put dummy value to sa[0] and sa[n+1], sa[1..n] is real suffix array of s
+    DOUBLE_SA = 1,              /**< allocate double required memory for suffix array */
+    SENTINEL = 2                /**< put dummy value to sa[0] and sa[n+1], sa[1..n] is real suffix array of s */
   };
-  
-  // print usage information
+
+  /** 
+   * print usage information
+   * 
+   * @param argc 
+   * @param argv 
+   */
   void print_usage(int argc, char * argv []);
 
-  // parse options and read/construct string & suffix array
-  // allocates memory if sa == 0. return memory for sa.
+  /** 
+   * parse options and read/construct string & suffix array
+   * allocates memory if sa == 0. return memory for sa.
+   * 
+   * @param argc 
+   * @param argv 
+   * @param s input text
+   * @param f option for suffix array
+   * @param sa pointer of suffix array
+   * 
+   * @return sa // does it need?
+   */
   int * Init(int argc, char * argv[], std::string & s, 
 	     unsigned int f = 0, int *sa = 0);
 
-  // read file fileName into string s.
+  /** 
+   * read file fileName into string s.
+   * 
+   * @param fileName file name
+   * @param s string that input text will be stored
+   */
   void stringFromFile(const std::string & fileName, std::string & s);
 
-  // calculate suffix array sa from s. allocates memory if sa == 0
-  // return *sa
+  /** 
+   * calculate suffix array sa from s. allocates memory if sa == 0
+   * 
+   * @param s input text
+   * @param sa suffix array
+   * @param f option for suffix array
+   * 
+   * @return sa
+   */
   int * suffixArray(const std::string & s, int * sa, unsigned int f);
 
-  // read suffix array of string s from fname + '.sa' into sa.
-  // if safname does not exist or seems invalid, create suffix array and save it
-  // to fname + '.sa'
-  // allocates memory if *sa == 0
-  // return *sa
+  /** 
+   * read suffix array of string s from fname + '.sa' into sa.
+   * if safname does not exist or seems invalid, create suffix array and save it
+   * to fname + '.sa'
+   * allocates memory if *sa == 0
+   * 
+   * @param s input text
+   * @param fname file name
+   * @param sa suffix array
+   * @param f option for suffix array
+   * 
+   * @return 
+   */
   int * saFromFile(const std::string & s, const std::string & fname, 
 		   int * sa, unsigned int f);
 
@@ -61,24 +83,57 @@ namespace LZShuffle {
   // computing lz from PSV, NSV in text order
   ////////////////////////////////////////////////////////////////////////////////
 
-  // for bgt9
+  /** 
+   * compute LZ factorization by using PSV and NSV
+   * 
+   * @param s input text
+   * @param psv psv array
+   * @param nsv nsv array
+   * @param lz lz factors
+   * 
+   * @return number of LZ factors
+   */
   unsigned int lzFromTOPNSV(const std::string & s, 
 		    const int * psv,
 		    const int * nsv,
 		    std::vector<std::pair<int,int> > * lz);
 
+
+  /** 
+   * compute LZ factorization by using NSV only
+   * the algorithm is based on the paper by Karkkainen et.al in CPM 2013.
+   * 
+   * @param s input text
+   * @param nsv nsv array
+   * @param n the length of s
+   * @param lz lz factors
+   * 
+   * @return number of LZ factors
+   */
   unsigned int lzFromTONSV2(const unsigned char * s, 
                             unsigned int * nsv,
                             unsigned int n,
                             std::vector<std::pair<int,int> > * lz);
 
 
-  // recover string from lz factorization
+  /** 
+   * recover string from lz factorization
+   * 
+   * @param lz lz factors
+   * 
+   * @return string represented by lz factors.
+   */
   std::string lz2str(const std::vector<std::pair<int,int> > & lz);
 
+  /** 
+   * for experiment
+   * 
+   * @return time
+   */
   double gettime();
 
   // read text by Simon Puglisi
+  // Copyright (c) 2013 Juha Karkkainen, Dominik Kempa and Simon J. Puglisi
   void read_text(const char *filename, unsigned char* &text, unsigned int &length);
 };
 #endif//__LZBG_COMMON_HPP__
